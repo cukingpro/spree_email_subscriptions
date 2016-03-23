4556d115594507a2b7510c066638f4c40ce059fd
Ha Huy <tongnguyen.hahuy@gmail.com>
2016-03-23 11:03:17 +0700

    a

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Product.class_eval do
-  def self.dishes_on_date(date)
+  def self.products_on_date(date)
     available_ons = Dish::AvailableOn.where(delivery_date: date)
     product_ids = available_ons.pluck(:product_id)
     products = Spree::Product.where(id: product_ids)
